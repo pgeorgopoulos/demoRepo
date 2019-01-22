f0c7cfb656aa6c746971fdaa6547f4f06d6e3149
Amneok <peter.g.georgopoulos@gmail.com>
2019-01-21 21:02:22 -0600

    trying params again

--- build/dockerBuild.sh
+++ build/dockerBuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
-APPENV=$1
-BUILD_ID=$2
+APPENV="$1"
+BUILD_ID="$2"
 
 docker build . --build-arg APPENV=$APPENV -t amneok/flaskapp3:v$BUILD_ID
 docker push amneok/flaskapp3:v$BUILD_ID
