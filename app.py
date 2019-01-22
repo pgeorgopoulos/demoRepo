b8c34e4c67960d00e11a4b80eb9a932d1251c994
amneok <peter.g.georgopoulos@gmail.com>
2019-01-22 22:42:37 +0000

    changing message

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def ts():
 @app.route("/")
 def mini_project():
     appenv = os.getenv('APPENV')
-    resp = jsonify({"message": "Free cake in " + appenv, "timestamp": ts()})
+    resp = jsonify({"message": "No better env than " + appenv, "timestamp": ts()})
     resp.headers["X-Frame-Options"] = "SAMEORIGIN"
     resp.headers["X-XSS-Protection"] = "1; mode=block"
     resp.headers["X-Content-Type-Options"] = "nosniff"
