f9befb7ab7946e69b2d3b9fd9ffb74db686d5856
Amneok <peter.g.georgopoulos@gmail.com>
2019-01-21 22:58:03 -0600

    adding route53 bit

---
+++ route53.py
@@ -0,0 +1,40 @@
+import boto3
+import argparse
+
+parser = argparse.ArgumentParser(description='Input the IP I need')
+parser.add_argument('--BUILD_ID', required=True)
+args = parser.parse_args()
+
+build_id = str(args.BUILD_ID).lower()
+
+
+cf = boto3.client('cloudformation')
+response = cf.describe_stacks(StackName='demoStack' + build_id)
+new_ip = response['Stacks'][0]['Outputs'][1]['OutputValue']
+
+r53 = boto3.client('route53')
+r53.change_resource_record_sets(
+    HostedZoneId='Z3JLKIWI026135',
+    ChangeBatch={
+        'Comment': 'Updating to new version',
+        'Changes': [
+            {
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': 'app',
+                    'Type': 'A',
+                    'ResourceRecords': [
+                        {
+                            'Value': new_ip
+                        },
+                    ],
+                    'AliasTarget': {
+                        'HostedZoneId': 'Z3JLKIWI026135',
+                        'DNSName': 'app.peter-g.net.',
+                        'EvaluateTargetHealth': True
+                    }
+                }
+            },
+        ]
+    }
+)
