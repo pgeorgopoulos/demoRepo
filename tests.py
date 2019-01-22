86c8b2200031620ab77b0c29836202920de2352b
Peter Georgopoulos <peter.g.georgopoulos@gmail.com>
2019-01-22 09:57:15 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 import time
-import requests
 from app import app
 
 
@@ -7,16 +6,6 @@ def test_app_file():
     with app.test_client() as c:
         rv = c.get('/')
         json_data = rv.get_json()
-        assert json_data["message"] == "Automation for the People", 'message does not equal "Automation for the People"'
-        now = time.time()
-        assert json_data["timestamp"] <= now, 'timestamp shows future time'
-        assert isinstance(json_data["timestamp"], int), 'timestamp not in correct integer format'
-        return 'OK'
-
-def test_app(ipaddr):
-        r = requests.get('http://' + ipaddr + ':5000/')
-        assert r.status_code == 200, 'status_code not 200'
-        json_data = r.json()
         assert json_data["message"] == "Automation for the People", 'message does not equal "Automation for the People"'
         now = time.time()
         assert json_data["timestamp"] <= now, 'timestamp shows future time'
