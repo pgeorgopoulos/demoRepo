55e0d0ea04b5eb445eeaa087da2fc801c3f5f0c5
Amneok <peter.g.georgopoulos@gmail.com>
2019-01-21 23:58:42 -0600

    please tell me I figured it out

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import time
+import os
 
 app = Flask(__name__)
 
--- dockerBuild.sh
+++ dockerBuild.sh
@@ -3,5 +3,5 @@
 APPENV="$1"
 BUILD_ID="$2"
 
-docker build . --build-arg APPENV=$APPENV -t amneok/flaskapp3:v$BUILD_ID
-docker push amneok/flaskapp3:v$BUILD_ID
+docker build . --build-arg APPENV=${APPENV} -t amneok/flaskapp3:v${BUILD_ID}
+docker push amneok/flaskapp3:v${BUILD_ID}
